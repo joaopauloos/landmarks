8d74ef7d7232f6be1bcd1712df5320bbb847eb40
Frederico Batista <jdjpaulo8@gmail.com>
2025-08-27 09:19:13 -0300

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by João Paulo on 27/08/25.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
